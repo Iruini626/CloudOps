02092463959ac61b771aa32ef11164a8e032bcdd
arielyip <arielyip@amazon.my>
2024-08-01 12:53:58 +0800

    unit test working

--- tests/unit/test_cloud_ops_stack.py
+++ tests/unit/test_cloud_ops_stack.py
@@ -1,15 +1,26 @@
-import aws_cdk as core
+import aws_cdk as cdk
 import aws_cdk.assertions as assertions
+from cloudops.cloudops_network_stack import CloudopsNetworkStack
 
-from cloudops.cloudops_network_stack import CloudOpsStack
-
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cloud_ops/cloud_ops_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CloudOpsStack(app, "cloud-ops")
+def test_vpc_creation():
+    app = cdk.App()
+    stack = CloudopsNetworkStack(app, "NetworkStack")
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    # Assert that the VPC is created
+    template.resource_count_is("AWS::EC2::VPC", 1)
+
+    # Assert VPC properties
+    template.has_resource_properties("AWS::EC2::VPC", {
+        "CidrBlock": "10.0.0.0/16",
+        "EnableDnsHostnames": True,
+        "EnableDnsSupport": True,
+        # Add more assertions as needed
+    })
+
+def test_vpc_connectivity():
+    # Create test instances, security groups, etc.
+    # Perform connectivity tests between resources
+    # Assert expected connectivity behavior
+    
+    pass
