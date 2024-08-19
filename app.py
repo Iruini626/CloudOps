8beebad3bf2d7cad1d5a084e7dc14ecff5cd633c
arielyip <arielyip@amazon.my>
2024-08-19 19:21:53 +0800

    Added Endpoints

--- app.py
+++ app.py
@@ -18,16 +18,20 @@ webserver_stack = CloudopsWebserverStack(app, "CloudOpsWebserverStack", vpc_main
 faulty_policy_stack = CloudopsFaultyPolicyStack(app, "CloudOpsFaultyPolicyStack", env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')))
 endpoints_stack = CloudOpsEndpointStack(app, "CloudOpsEndpointStack", vpc_main=network_stack.vpc, env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')))
 
+# Feel free to rename tags
 cdk.Tags.of(app).add('environment','prod')
 cdk.Tags.of(app).add('application','cloudops')
 cdk.Tags.of(app).add('owner', 'arielyip')
 cdk.Tags.of(app).add('deployment_version','1.3')
-# Comment out the folllowing line to disable registration of myApplications
+# Comment out the folllowing line to disable registration of myApplications, myApplications resource needs to be created first
 cdk.Tags.of(app).add("awsApplication", "arn:aws:resource-groups:ap-southeast-1:024444663664:group/cloudops/05cyy2riymuymc7ei8iqk94dt9")
+
+# This adds particular tag for resources based on their stack
 cdk.Tags.of(network_stack).add('resource_group','network_layer')
 cdk.Tags.of(inventory_stack).add('resource_group', 'inventory_layer')
 cdk.Tags.of(peering_stack).add('resource_group', 'peering_layer')
 cdk.Tags.of(webserver_stack).add('resource_group', 'webserver_layer')
 cdk.Tags.of(faulty_policy_stack).add('resource_group', 'faulty_policy_layer')
+cdk.Tags.of(endpoints_stack).add('resource_group', 'endpoints_layer')
 
 app.synth()
--- cloudops/cloudops_endpoints_stack.py
+++ cloudops/cloudops_endpoints_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
-    aws_ec2 as ec2
+    aws_ec2 as ec2,
+    CfnOutput
 )
 from constructs import Construct
 
@@ -9,17 +10,26 @@ class CloudOpsEndpointStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, vpc_main, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Create endpoint security group 
+        endpoint_security_group = ec2.SecurityGroup(self, "EndpointSecurityGroup",
+                                                    vpc=vpc_main,
+                                                    description="Security Group for VPC Endpoints",
+                                                    allow_all_outbound=True)
+        endpoint_security_group.add_ingress_rule(ec2.Peer.ipv4(vpc_main.vpc_cidr_block), ec2.Port.tcp(443))
+
         # Create endpoints for session manager in private subnet
         ssm_endpoint = ec2.InterfaceVpcEndpoint(self, "SSMEndpoint",
                                                 vpc=vpc_main,
                                                 service=ec2.InterfaceVpcEndpointAwsService.SSM,
                                                 private_dns_enabled=True,
+                                                security_groups = [endpoint_security_group],
                                                 subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS))
         
         ec2_messages = ec2.InterfaceVpcEndpoint(self, "EC2Messages",
                                                 vpc=vpc_main,
                                                 service=ec2.InterfaceVpcEndpointAwsService.EC2_MESSAGES,
                                                 private_dns_enabled=True,
+                                                security_groups = [endpoint_security_group],
                                                 subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
                                                 )
         
@@ -27,7 +37,17 @@ class CloudOpsEndpointStack(Stack):
                                                 vpc=vpc_main,
                                                 service=ec2.InterfaceVpcEndpointAwsService.SSM_MESSAGES,
                                                 private_dns_enabled=True,
+                                                security_groups = [endpoint_security_group],
                                                 subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
                                                 )
         
+        # Create an EC2 to execute CLI Commands
+        command_host = ec2.Instance(self, "CommandHost",
+                                    instance_type=ec2.InstanceType("t4g.nano"),
+                                    machine_image=ec2.MachineImage.latest_amazon_linux2023(),
+                                    security_group = endpoint_security_group,
+                                    vpc=vpc_main,
+                                    vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
+                                    )
         
+        instance_id = CfnOutput(self, "InstanceId", value=command_host.instance_id)
